a9c8c8c504fd4ef0414f62db0f93a85782292aca
irishlouis <louissmith@gmail.com>
2016-02-24 11:12:39 +0000

    added confusion matrix

--- find teeth brushing pattern.R
+++ find teeth brushing pattern.R
@@ -312,7 +312,23 @@ plot.similarity <- function(d){
 
 plot.similarity(test.results)
 
+###################################################################
+#
+# evaluation confusion matrix
 
+require(caret)
+brushing.minutes <- c(dmy_hms("20/01/2016 170200", tz = "GMT"),
+                      dmy_hms("20/01/2016 170300", tz = "GMT"),
+                      dmy_hms("20/01/2016 180600", tz = "GMT"),
+                      dmy_hms("20/01/2016 180700", tz = "GMT"),
+                      dmy_hms("20/01/2016 180800", tz = "GMT"),
+                      dmy_hms("20/01/2016 182700", tz = "GMT"),
+                      dmy_hms("20/01/2016 182800", tz = "GMT"),
+                      dmy_hms("20/01/2016 182900", tz = "GMT"),
+                      dmy_hms("20/01/2016 215600", tz = "GMT"),
+                      dmy_hms("20/01/2016 215700", tz = "GMT"))
+times <- unique(test$time_minute)
+confusionMatrix(ifelse(sim.results$event > 0, 1, 0), ifelse(times %in% brushing.minutes, 1, 0))
 
 
 ###################################################################
