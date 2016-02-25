3b28ac785a194cb7faf7478fa6dd16cc1f72177d
irishlouis <louissmith@gmail.com>
2016-02-25 17:17:55 +0000

    end of day 20160225

--- test_brushing_pattern.R
+++ test_brushing_pattern.R
@@ -12,14 +12,17 @@ require(reshape2)
 require(doParallel)
 require(data.table)
 
-test.df <- fread("./TAS1E35150309 (2016-01-21)RAW.csv", stringsAsFactors = F, skip = 10, header = T, data.table = F)
-test.new <- fread("./TAS1E35150309 (2016-01-21)RAW.csv", stringsAsFactors = F, skip = 10, header = T, data.table = F)
+org.df <- fread("TAS1E35150309 (2016-01-21)RAW.csv", stringsAsFactors = F, skip = 10, header = T, data.table = F)
+# marie's data
+test.df <- fread("./test_data/TAS1E31150005_2016-02-25___09-40-14RAW.csv", stringsAsFactors = F, skip = 10, header = T, data.table = F)
+# louis data
+test.new <- fread("./test_data/TAS1E31150003_2016-02-25___09-42-28RAW.csv", stringsAsFactors = F, skip = 10, header = T, data.table = F)
 
 #' format.df
 #'
-#' @param df - original data.frame to be formated 
+#' @param df - raw actigraphy data to be formated, without metadata 
 #'
-#' @return df - formated df
+#' @return df - formated df with formated time, vector.mag & vector.dir
 #' 
 format.df <- function(df) {
   names(df) <- str_replace(names(df), " ", "")
@@ -41,20 +44,21 @@ format.df <- function(df) {
   return(df)
 }
 
+# format the data
+org.df <- format.df(org.df)
 test.df <- format.df(test.df)
 test.new <- format.df(test.new)
 
 
-#' Title
+#' get.peak.summary
 #'
-#' @param v 
-#' @param k 
+#' @param v - vector to be smoothed / considered
+#' @param k - rolling average
+#' @param freq - frequency device was recording at
 #'
-#' @return
-#' @export
-#'
-#' @examples
-get.peak.summary <- function(v, k = 10) {
+#' @return vector summarising the peak for v
+#' 
+get.peak.summary <- function(v, k, freq) {
   require(zoo)
   v.smooth <- rollapply(v, k, mean)
   switch.dir <- sapply(seq_along(v.smooth), 
@@ -64,32 +68,34 @@ get.peak.summary <- function(v, k = 10) {
   peaks.per.sec <- sum(sapply(seq_along(v.smooth), 
                               function(x) ifelse(x<length(v.smooth), 
                                                  switch.dir[x] != switch.dir[x+1], 
-                                                 F))) / (length(v.smooth)/100)
+                                                 F))) / (length(v.smooth)/freq)
   if(peaks.per.sec == 0) return(F)
   
   period <- rollapply(which(sapply(seq_along(v.smooth), 
                                    function(x) ifelse(x<length(v.smooth), 
                                                       switch.dir[x] != switch.dir[x+1], 
                                                       F)) == T),
-                      2, function(x) x[2] - x[1])
+                      2, function(x) (x[2] - x[1])/freq)
   
   avg.period <- mean(period)
   sd.period <- sd(period)
   return(c(peaks.per.sec = peaks.per.sec, avg.period = avg.period, sd.period = sd.period))
 }
 
-#' Title
+#' peak.func
 #'
-#' @param t 
+#' @param t time period to calculate peak summary for
+#' @param df dataset to pull vector from
+#' @param freq frequency device recording at 
+#' @param k window size for smoothing
 #'
-#' @return
-#' @export
-#'
-#' @examples
-peak.func <- function(t, test){
-  tbl <- test %>% filter(time_minute == t) %>% 
+#' @return data.frame of peak summary for period t
+#' 
+peak.func <- function(t, df, freq, k){
+  tbl <- df %>% filter(time_minute == t) %>% 
     select(-Timestamp, -vector.dir, -time_minute) %>% 
-    apply(.,2, get.peak.summary) %>% t() %>% data.frame %>% mutate(Timestamp = t) 
+    apply(.,2, function(x) get.peak.summary(v = x, k = k, freq = freq)) %>% 
+    t() %>% data.frame %>% mutate(Timestamp = t) 
   if(nrow(tbl) == 4) {
     return(tbl)
   } else{
@@ -100,15 +106,15 @@ peak.func <- function(t, test){
   }
 }
 
-#' Title
+#' summary.df
 #'
-#' @param df 
+#' @param df data.frame to summarise
+#' @param freq frequency device recording at
+#' @param k window size of smoothing
 #'
-#' @return
-#' @export
-#'
-#' @examples
-summary.df <- function(df){
+#' @export summary data.frame
+#' 
+summary.df <- function(df, freq, k=10){
   test <- df
   times <- unique(test$time_minute)
   
@@ -125,64 +131,62 @@ summary.df <- function(df){
     left_join(select(test, Timestamp, vector.dir, -time_minute) %>% 
                 filter(vector.dir == 5) %>%
                 mutate(Timestamp = floor_date(Timestamp, "minute")) %>% 
-                group_by(Timestamp) %>% summarise(per.vec.dir5 = n()/6000),
+                group_by(Timestamp) %>% summarise(per.vec.dir5 = n()/(60 * freq)),
               by = "Timestamp"
     )
-  tmp <- do.call(rbind, lapply(times, function(t) peak.func(t, test)))
+  tmp <- do.call(rbind, lapply(times, function(t) peak.func(t, test, freq, k)))
   test.summary <- cbind(test.summary, tmp %>% select(-Timestamp)) 
   return(test.summary)
 }
 
-test.df.summary <- summary.df(test.df)
-test.new.summary <- summary.df(test.new)
+org.df.summary   <- summary.df(org.df, freq = 100, k=10)    # org data used to generate fingerprint
+test.df.summary  <- summary.df(df = test.df, freq = 30, k=3)    # marie
+test.new.summary <- summary.df(test.new, freq = 30, k=3)   # louis
 
-
-
-#' Title
+#' similarity
 #'
 #' @param brushing.fingerprint 
 #' @param d 
 #'
 #' @return
 #' @export
-#'
-#' @examples
+#' 
 similarity <- function(brushing.fingerprint, d){
   return(
     sqrt(sum(((brushing.fingerprint$Qu1st-d$Qu1)/brushing.fingerprint$Qu1st)^2)) +
       sqrt(sum(((brushing.fingerprint$Median-d$Median)/brushing.fingerprint$Median)^2)) +
       sqrt(sum(((brushing.fingerprint$Mean-d$Mean)/brushing.fingerprint$Mean)^2)) +
-      sqrt(sum(((brushing.fingerprint$X3rd.Qu.-d$Qu2)/brushing.fingerprint$X3rd.Qu.)^2)) +
-      sqrt(sum(((brushing.fingerprint$mean.dir5[4]-max(d$per.vec.dir5[4], 0, na.rm = T))/brushing.fingerprint$mean.dir5[4])^2)) +
-      sqrt(sum(((brushing.fingerprint$peaks.per.sec - d$peaks.per.sec)/brushing.fingerprint$peaks.per.sec)^2)) +
+      sqrt(sum(((brushing.fingerprint$Qu3rd-d$Qu2)/brushing.fingerprint$Qu3rd)^2)) +
+      # sqrt(sum(((brushing.fingerprint$mean.dir5[4]-max(d$per.vec.dir5[4], 0, na.rm = T))/brushing.fingerprint$mean.dir5[4])^2)) +
       sqrt(sum(((brushing.fingerprint$avg.period - d$avg.period)/brushing.fingerprint$avg.period)^2)) +
-      sqrt(sum(((brushing.fingerprint$sd.period - d$sd.period)/brushing.fingerprint$sd.period)^2)) 
+      sqrt(sum(((brushing.fingerprint$sd.period - d$sd.period)/brushing.fingerprint$sd.period)^2)) +
+      sqrt(sum(((brushing.fingerprint$peaks.per.sec - d$peaks.per.sec)/brushing.fingerprint$peaks.per.sec)^2))
   )
 }
 
-
-#' Title
+#' get.sim.results
 #'
-#' @param df 
+#' @param raw.df -
+#' @param summary.df 
 #' @param brushing.fingerprint 
+#' @param similarity 
+#' @param close
 #'
 #' @return
-#' @export
-#'
-#' @examples
-get.sim.results <- function(org.df, df, brushing.fingerprint, similarity){
+#' 
+get.sim.results <- function(raw.df, summary.df, brushing.fingerprint, similarity, close = 2){
   cores <- detectCores()
   cl <- makeCluster(cores)
   registerDoParallel(cl)
-  times <- unique(df$Timestamp)
+  times <- unique(summary.df$Timestamp)
   
   sim.results <- foreach(t = seq_along(times), .combine = c) %dopar% {
-    return(sim = similarity(as.data.frame(brushing.fingerprint), df[df$Timestamp == times[t], ]))
+    return(sim = similarity(as.data.frame(brushing.fingerprint)[4,], summary.df[summary.df$Timestamp == times[t], ][4,]))
   }
   stopCluster(cl)
 
   sim.results <- data.frame(times = times, sim = sim.results)
-  sim.results$event <- ifelse(sim.results$sim < 2, 1, 0) 
+  sim.results$event <- ifelse(sim.results$sim < close, 1, 0) 
 
   counter <- 0
   for (i in 2:length(sim.results$event)) {
@@ -191,17 +195,52 @@ get.sim.results <- function(org.df, df, brushing.fingerprint, similarity){
   }
   message(paste(counter, " events of brushing teeth have been identified"))
 
-  result <- left_join(org.df, 
+  result <- left_join(raw.df, 
                       sim.results, 
                       by = c("time_minute" = "times"))
 
   return(result)
 }
 
-test.df <- get.sim.results(org.df = test.df, 
-                df = test.df.summary, 
-                brushing.fingerprint, 
-                similarity)
+# save.image("test_brushing_pattern.RDATA")
+
+org.result <- get.sim.results(raw.df = org.df, close = 0.5, 
+                               summary.df = org.df.summary, 
+                               brushing.fingerprint, 
+                               similarity)
+org.result %>% select(time_minute, sim,  event) %>% filter(event > 0) %>% distinct()
+
+test.result <- get.sim.results(raw.df = test.df,
+                               close = 0.5,
+                               summary.df = test.df.summary, 
+                               brushing.fingerprint, 
+                               similarity)
+test.result %>% select(time_minute, sim,  event) %>% filter(event > 0) %>% distinct()
+
+test.result %>% filter(Timestamp == ymd_hms("20160224 213900")) %>%
+  select(time_minute, sim, event)  %>% distinct()
+
+test.df.summary %>% filter(Timestamp == ymd_hms("20160224 213900"))
+
+ggplot(test.df %>% filter(Timestamp > ymd_hms("20160224 213900"), Timestamp <= ymd_hms("20160224 213901")),
+       aes(x = 1:30)) + geom_line(aes(y = vector.mag), col = "blue") 
+
+
+new.result <- get.sim.results(raw.df = test.new, close = .5,
+                              summary.df = test.new.summary, 
+                              brushing.fingerprint, 
+                              similarity)
+
+new.result %>% select(time_minute, sim,  event) %>% filter(event > 0) %>% distinct()
+
+new.result %>% filter(Timestamp > ymd_hms("20160225 072500"), Timestamp < ymd_hms("20160225 072700")) %>%
+  select(time_minute, sim, event)  %>% distinct()
+
+ggplot(test.new %>% filter(Timestamp > ymd_hms("20160224 213900"), Timestamp < ymd_hms("20160224 214000")),
+       aes(x= Timestamp)) + geom_line(aes(y=vector.mag), col= "red")
+
+test.new.summary %>% filter(Timestamp == ymd_hms("20160224 213900"))
+
 
 
 #' Title
