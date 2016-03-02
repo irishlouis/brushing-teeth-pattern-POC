61422712d0f2177baa8004c8754df422fefbda62
irishlouis <louissmith@gmail.com>
2016-03-02 08:50:26 +0000

    end of day 20160301

--- find teeth brushing pattern.R
+++ find teeth brushing pattern.R
@@ -70,12 +70,12 @@ df4 <- df %>%
 plot.data <- function(d){
   p1 <- ggplot(d, aes(Timestamp, vector.mag)) + geom_point(aes(col = vector.dir)) + 
     geom_line() + theme(legend.position="none") + labs(title = "Overall Vector Mag")
-  p <- ggplot(d, aes(Timestamp, as.numeric(as.character(vector.dir)))) + geom_line() + labs(title = "Vector Dir")
+ # p <- ggplot(d, aes(Timestamp, as.numeric(as.character(vector.dir)))) + geom_line() + labs(title = "Vector Dir")
   p2 <- ggplot(d, aes(Timestamp, AccelerometerX)) + geom_line() + labs(title = "X Accel Data")
   p3 <- ggplot(d, aes(Timestamp, AccelerometerY)) + geom_line() + labs(title = "Y Accel Data")
   p4 <- ggplot(d, aes(Timestamp, AccelerometerZ)) + geom_line() + labs(title = "Z Accel Data")
   
-  grid.arrange(p1, p, p2, p3, p4, ncol = 1)
+  grid.arrange(p1, p2, p3, p4, ncol = 1)
   }
 
 plot.data(df1)
